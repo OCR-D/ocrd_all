fa2a47715f56b0f64d19db0e8fbca692189d198e
joschrew <91774427+joschrew@users.noreply.github.com>
2023-07-12 17:15:56 +0200

    Remove metspath-check from delegator-template
    Co-authored-by: Robert Sachunsky <38561704+bertsky@users.noreply.github.com>

--- slim-containers-files/delegator_template.py
+++ slim-containers-files/delegator_template.py
@@ -8,13 +8,6 @@ processing_server_address = "http://localhost:8000"
 processor_name = "{{ processor_name }}"
 
 args = list(sys.argv)
-if "-m" in args:
-    idx = args.index("-m")
-    metspath = args[idx + 1]
-    if Path(metspath).is_absolute():
-        print("absolute path is not supported")
-        exit(1)
-    args[idx + 1] = f"/data/{metspath}"
 
 
 cmd = [
