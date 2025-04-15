20f4937bac3740eae570b3519ba4e3a48098cde3
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2025-04-15 17:31:45 +0200

    generalise+rename ocrd-all-dockerhub → ocrd-all-meta

--- ocrd-all-dockerhub.py
+++
@@ -1,9 +0,0 @@
-import json
-import sys
-all = dict()
-for path in sys.argv[1:]:
-    tool = json.load(open(path))
-    dockerhub = tool.get('dockerhub', '')
-    for executable in tool['tools']:
-        all[executable] = dockerhub
-print(json.dumps(all, indent=2))
